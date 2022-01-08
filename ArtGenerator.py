053d176c4127ea3eef6f6fbf68c38534a81dd861
Happyfacemike <michael.janeway03@gmail.com>
2022-01-08 12:00:25 -0500

    Formatted the code

--- ArtGenerator.py
+++ ArtGenerator.py
@@ -4,6 +4,7 @@ import math
 from random import randint, random, sample, choice
 from PIL import Image, ImageDraw, ImageTk, ImageColor, ImageGrab
 
+
 class ArtGenerator:
     def __init__(self, root):
         self.root = root
@@ -11,44 +12,56 @@ class ArtGenerator:
 
         self.frame = ttk.Frame(self.root, padding="20")
         self.frame.grid(column=0, row=0, sticky=(N, W, E, S))
-        self.root.columnconfigure(0, weight = 1)
-        self.root.rowconfigure(0, weight=1) #All of these previous lines of code are essentially setting up the frame and window, and how they dynamically resize
+        self.root.columnconfigure(0, weight=1)
+        # All of these previous lines of code are essentially setting up the frame and window, and how they dynamically resize
+        self.root.rowconfigure(0, weight=1)
 
-        self.drawInputWidgets() #this basically draws all the labels and input widgets needed to get user input to draw the random painting 
+        # this basically draws all the labels and input widgets needed to get user input to draw the random painting
+        self.drawInputWidgets()
 
-        
+    # this was created to simplify __init__ by making it so that the input widgets would be put in another function for readability
 
-    def drawInputWidgets(self): #this was created to simplify __init__ by making it so that the input widgets would be put in another function for readability
-        self.artLabel = ttk.Label(self.frame, text = "Random Art Generator", font= "helvetica 14 bold")
-        self.artLabel.grid(column= 0, row = 0, columnspan= 4, padx=5)
+    def drawInputWidgets(self):
+        self.artLabel = ttk.Label(
+            self.frame, text="Random Art Generator", font="helvetica 14 bold")
+        self.artLabel.grid(column=0, row=0, columnspan=4, padx=5)
 
-        self.squareSidesLabel = ttk.Label(self.frame, text="Number of Squares on Each Side", font = "helvetica 10 bold")
-        self.squareSidesLabel.grid(column = 0, row = 1, columnspan= 3, pady=15) #setting up info labels
+        self.squareSidesLabel = ttk.Label(
+            self.frame, text="Number of Squares on Each Side", font="helvetica 10 bold")
+        # setting up info labels
+        self.squareSidesLabel.grid(column=0, row=1, columnspan=3, pady=15)
 
-        #Now creating a scale to get user input on the amount of squares on each side -- might need a lambda function to fix decimal issue
-        self.squareSidesFloat = StringVar() #holding floats of numeric values
+        # Now creating a scale to get user input on the amount of squares on each side -- might need a lambda function to fix decimal issue
+        self.squareSidesFloat = StringVar()  # holding floats of numeric values
         self.squareSidesFloat.set('1')
 
-        self.squareSidesScale = ttk.Scale(self.frame, orient=HORIZONTAL, length=200, from_=1, to= 20, value=1, command=lambda s: self.squareSidesFloat.set(math.floor(float(s)))) #learned a lot of that command from stackOverFlow (adjusted it a bit)
-        self.squareSidesScale.grid(column = 0, row = 2, pady = 25, padx = 20, rowspan= 4)
+        self.squareSidesScale = ttk.Scale(self.frame, orient=HORIZONTAL, length=200, from_=1, to=20, value=1, command=lambda s: self.squareSidesFloat.set(
+            math.floor(float(s))))  # learned a lot of that command from stackOverFlow (adjusted it a bit)
+        self.squareSidesScale.grid(
+            column=0, row=2, pady=25, padx=20, rowspan=4)
 
-        self.squareSidesLabel = ttk.Label(self.frame, textvariable= self.squareSidesFloat)
-        self.squareSidesLabel.grid(column=0, row=6, pady=10, rowspan = 10) #added these rows at the end to space out the button a lot
+        self.squareSidesLabel = ttk.Label(
+            self.frame, textvariable=self.squareSidesFloat)
+        # added these rows at the end to space out the button a lot
+        self.squareSidesLabel.grid(column=0, row=6, pady=10, rowspan=10)
 
-        self.generateArtButton = ttk.Button(self.frame, text="Create Artwork", command=lambda: self.create_squares())
-        self.generateArtButton.grid(row = 16, column=0, pady = (200, 0), ipady = 10, ipadx = 20, rowspan=8) #trying to pad this a lot so  that we have a nice big space between the scale and button
-    
+        self.generateArtButton = ttk.Button(
+            self.frame, text="Create Artwork", command=lambda: self.create_squares())
+        # trying to pad this a lot so  that we have a nice big space between the scale and button
+        self.generateArtButton.grid(row=16, column=0, pady=(
+            200, 0), ipady=10, ipadx=20, rowspan=8)
+
         # Canvas
         self.cv = Canvas(self.frame, width=720, height=720)
         self.cv.grid(row=0, column=4, rowspan=24)
 
         self.images = []
-    
+
     def create_squares(self):
         self.cv.delete('all')
 
         degree = int(self.squareSidesFloat.get())
-        square_points = self.divide_square([[0,0], [720, 720]], degree, 10)
+        square_points = self.divide_square([[0, 0], [720, 720]], degree, 10)
 
         for square in square_points:
             # self.cv.create_rectangle(square[0][0], square[0][1], square[1][0], square[1][1], fill="blue")  # FOR TESTING ONLY
@@ -60,21 +73,21 @@ class ArtGenerator:
         self.add_triangle(square)
 
         #self.create_polygon(10, 10, 10, 20, 200, 300, 250, 150, 10, 10, fill="blue", alpha=0.5)
-    
+
     # Divides a given square into 'degree' pieces
     # Increase spacing to add space between squares, Decrease to add overlap
-    def divide_square(self, square, degree, spacing= 1): 
+    def divide_square(self, square, degree, spacing=1):
         new_square_width = int(abs(square[0][0] - square[1][0])/(degree))
         square_points = []
         for x in range(square[0][0], square[1][0], new_square_width):
             for y in range(square[0][1], square[1][1], new_square_width):
                 square_points.append([[int(x+new_square_width/spacing), int(y+new_square_width/spacing)], [
                     int(x+new_square_width-new_square_width/spacing), int(y+new_square_width-new_square_width/spacing)]])
-                
+
         return square_points
 
-
     # https://stackoverflow.com/questions/62117203/how-to-make-a-tkinter-canvas-polygon-transparent
+
     def create_polygon(self, *args, **kwargs):
         if "alpha" in kwargs:
             if "fill" in kwargs:
@@ -89,7 +102,8 @@ class ArtGenerator:
                 # and they also are the width and height of it respectively (the image will be inserted into
                 # (0, 0) coords for simplicity)
                 image = Image.new("RGBA", (max(args[::2]), max(args[1::2])))
-                ImageDraw.Draw(image).polygon(args, fill= choice(list(ImageColor.colormap.items()))[0], outline=outline)
+                ImageDraw.Draw(image).polygon(args, fill=choice(
+                    list(ImageColor.colormap.items()))[0], outline=outline)
 
                 # prevent the Image from being garbage-collected
                 self.images.append(ImageTk.PhotoImage(image))
