9b2389a2c023a47acbe6ebb0ebe73f670a319793
Isaiah Sinclair <89956249+Isss11@users.noreply.github.com>
2022-01-08 00:59:23 -0500

    Fixed Scale Issue
    I had a problem with the scale where it wasn't producing integer values (which I needed) -- that is fixed now, but I think I still need to find a way to save the value.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from ArtGenerator import *
+from ArtGenerator import * 
 
-def main():
+def main(): #NOTE this main function was created purely to test ArtGenerator.py -- it will probably be deleted once code starts to get merged together
     root = Tk()
     ArtGenerator(root)
 
