9dcc49953f9452daaa0787f02d0c0ce1333ed14d
Happyfacemike <michael.janeway03@gmail.com>
2022-01-08 13:34:59 -0500

    Update ArtGenerator.py

--- ArtGenerator.py
+++ ArtGenerator.py
@@ -16,67 +16,60 @@ class ArtGenerator:
         # All of these previous lines of code are essentially setting up the frame and window, and how they dynamically resize
         self.root.rowconfigure(0, weight=1)
 
-        # this basically draws all the labels and input widgets needed to get user input to draw the random painting
-        self.drawInputWidgets()
+        self.createLabels()
+        self.createInputWidgets()
 
-    # this was created to simplify __init__ by making it so that the input widgets would be put in another function for readability
-
-    def drawInputWidgets(self):
-        self.artLabel = ttk.Label(
-            self.frame, text="Random Art Generator", font="helvetica 14 bold")
+    # Creates GUI Labels
+    def createLabels(self):
+        self.artLabel = ttk.Label(self.frame, text="Random Art Generator", font="helvetica 14 bold")
         self.artLabel.grid(column=0, row=0, columnspan=4, padx=5)
-
-        self.squareSidesLabel = ttk.Label(
-            self.frame, text="Number of Squares on Each Side", font="helvetica 10 bold")
-        # setting up info labels
+        
+        self.squareSidesLabel = ttk.Label(self.frame, text="Number of Squares on Each Side", font="helvetica 10 bold")
         self.squareSidesLabel.grid(column=0, row=1, columnspan=3, pady=15)
+        
+        self.spacingTitle = ttk.Label(self.frame, text="Enter Factor of Spacing", font="helvetica 10 bold")
+        self.spacingTitle.grid(column=0, row=8, columnspan=3, pady=15)
 
-        # Now creating a scale to get user input on the amount of squares on each side -- might need a lambda function to fix decimal issue
-        self.squareSidesFloat = StringVar()  # holding floats of numeric values
-        self.squareSidesFloat.set('1')
+    # Creates Input Widgets and Canvas
+    def createInputWidgets(self):
+        
+        # Degree of art squares
+        # 1 means 1x1 grid, 3 means 3x3 grid, etc. 
+        self.squareDegree = StringVar()
+        self.squareDegree.set('1')
 
-        self.squareSidesScale = ttk.Scale(self.frame, orient=HORIZONTAL, length=200, from_=1, to=20, value=1, command=lambda s: self.squareSidesFloat.set(
-            math.floor(float(s))))  # learned a lot of that command from stackOverFlow (adjusted it a bit)
+        self.squareSidesScale = ttk.Scale(self.frame, orient=HORIZONTAL, length=200, from_=1, to=20, value=1, command=lambda s: self.squareDegree.set(math.floor(float(s))))  # learned a lot of that command from stackOverFlow (adjusted it a bit)
         self.squareSidesScale.grid(column=0, row=2, pady=(20, 0), padx=20)
 
-        self.squareSidesLabel = ttk.Label(self.frame, textvariable=self.squareSidesFloat)
-        # added these rows at the end to space out the button a lot
+        self.squareSidesLabel = ttk.Label(self.frame, textvariable=self.squareDegree)
         self.squareSidesLabel.grid(column=0, row=3, pady=10, rowspan=5)
 
-        #Creating scale and labels to get the degree of spacing
-        #Starting with the title
+        self.squareSpacing = StringVar()
+        
+        self.spacingScale = ttk.Scale(self.frame, orient= HORIZONTAL, length = 200, from_=0.1, to=15, command=lambda s: self.squareSpacing.set(float(s)))
+        self.spacingScale.set(10)
+        self.spacingScale.grid(column = 0, row=9, pady= 10, rowspan=2)
 
-        self.degreeTitle = ttk.Label(self.frame, text="Enter a Degree of Spacing", font="helvetica 10 bold")
-        self.degreeTitle.grid(column=0, row=8, columnspan=3, pady=15)
+        # Creating a label to write out the value of the scale variable (self.degree)
+        self.spacingValueText = ttk.Label(self.frame, textvariable=self.squareSpacing)
+        self.spacingValueText.grid(column = 0, row= 11, pady= 10)
 
-        #Now Creating the scale and the degree variable
-        self.degree = StringVar()
-        self.degree.set('10.0')
-
-        self.degreeScale = ttk.Scale(self.frame, orient= HORIZONTAL, length = 200, from_=0.1, to=10, command= lambda t: self.degree.set(round(float(t), 2))) #Created scale
-        self.degreeScale.grid(column = 0, row=9, pady= 10, rowspan=2)
-
-        #Creating a label to write out the value of the scale variable (self.degree)
-        self.degreeValueText = ttk.Label(self.frame, textvariable=self.degree)
-        self.degreeValueText.grid(column = 0, row= 11, pady= 10)
-
-
-        self.generateArtButton = ttk.Button(
-            self.frame, text="Create Artwork", command=lambda: self.create_squares())
-        # trying to pad this a lot so  that we have a nice big space between the scale and button
+        self.generateArtButton = ttk.Button(self.frame, text="Create Artwork", command=lambda: self.create_squares())
         self.generateArtButton.grid(row=16, column=0, pady=(200, 0), ipady=10, ipadx=20, rowspan=8)
 
         # Canvas
         self.cv = Canvas(self.frame, width=720, height=720, highlightbackground="gray49")
         self.cv.grid(row=0, column=4, rowspan=24)
 
+        # Contains all images used in the artwork
         self.images = []
 
     def create_squares(self):
         self.cv.delete('all')
 
-        degree = int(self.squareSidesFloat.get())
-        square_points = self.divide_square([[0, 0], [720, 720]], degree, 10)
+        degree = int(self.squareDegree.get())
+        spacing = float(self.squareSpacing.get())
+        square_points = self.divide_square([[0, 0], [720, 720]], degree, spacing)
 
         for square in square_points:
             # self.cv.create_rectangle(square[0][0], square[0][1], square[1][0], square[1][1], fill="blue")  # FOR TESTING ONLY
