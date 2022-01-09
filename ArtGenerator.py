d1d2fcd53bbe27f20a316b59933b62541cd32768
Happyfacemike <michael.janeway03@gmail.com>
2022-01-08 21:14:21 -0500

    Art generator Upgrade

--- ArtGenerator.py
+++ ArtGenerator.py
@@ -33,12 +33,14 @@ class Picture:
         self.add_quartercircle(square)
         
         if randint(1, 3) == 3:
+            pass
             self.add_circle(square)
         
         miniSquares = self.divide_square(square, 2)
         
         for miniSquare in miniSquares:
             if randint(1, 10) > 7:
+                pass
                 self.add_triangle(miniSquare)
 
     # Divides a given square into 'degree' pieces
@@ -68,23 +70,28 @@ class Picture:
 
         p_start = randint(0, 3)
         
-        if randint(0, 1):
+        if randint(0, 1): # Corner Triangle
             self.ctx.move_to(points[p_start][0],points[p_start][1])
             self.ctx.line_to(points[p_start + 2][0], points[p_start + 2][1])
             self.ctx.line_to(points[p_start + 1][0], points[p_start + 1][1])
-            self.ctx.fill()
+            self.ctx.line_to(points[p_start][0],points[p_start][1])
         else:  # Side Triangle
             self.ctx.move_to(points[p_start][0], points[p_start][1])
             self.ctx.line_to(center[0], center[1])
             self.ctx.line_to(points[p_start + 1][0], points[p_start + 1][1])
-            self.ctx.fill()
+            self.ctx.line_to(points[p_start][0], points[p_start][1])
+        
+        self.s_or_f()
+
     
     def add_circle(self, square):
         center = [int((square[0][0] + square[1][0]) / 2),
                   int((square[0][1] + square[1][1]) / 2)]
         circle_width = int(math.ceil(abs(square[0][0] - square[1][0])))/2
         
-        #self.ctx.arc(center[0], center[1],circle_width, 0, 2*math.pi)
+        self.ctx.arc(center[0], center[1],circle_width, 0, 2*math.pi)
+        self.s_or_f()
+
     
     def add_semicircle(self, square):
         self.colour_switch()
@@ -108,6 +115,8 @@ class Picture:
             self.ctx.arc(p3[0] + circle_width, p3[1], circle_width, math.pi, 0)
         elif x == 4: # Left
             self.ctx.arc(p3[0], p3[1] - circle_width, circle_width, math.pi/2*3, math.pi/2)
+            
+        self.s_or_f()
     
     def add_quartercircle(self, square):
         self.colour_switch()
@@ -124,18 +133,46 @@ class Picture:
         x = randint(1,5)
         
         if x == 1: # Top Right
-            self.ctx.arc(p2[0], p2[1], circle_width, 0, math.pi)
+            self.ctx.move_to(p2[0], p2[1])
+            self.ctx.line_to(p2[0] - circle_width, p2[1])
+            self.ctx.move_to(p2[0], p2[1])
+            self.ctx.line_to(p2[0], p2[1] + circle_width)
+            self.ctx.arc(p2[0], p2[1], circle_width, math.pi/2, math.pi)
+            
         elif x == 2: # Bottom Right
-            self.ctx.arc(p2[0], p2[1] + circle_width, circle_width, math.pi/2, math.pi/2*3)
+            self.ctx.move_to(p4[0], p4[1])
+            self.ctx.line_to(p4[0], p4[1] - circle_width)
+            self.ctx.move_to(p4[0], p4[1])
+            self.ctx.line_to(p4[0] - circle_width, p4[1])
+            self.ctx.arc(p4[0], p4[1], circle_width, math.pi, math.pi/2*3)
+            
         elif x == 3: # Bottom Left
-            self.ctx.arc(p3[0] + circle_width, p3[1], circle_width, math.pi, 0)
+            self.ctx.move_to(p3[0], p3[1])
+            self.ctx.line_to(p3[0] + circle_width, p3[1])
+            self.ctx.move_to(p3[0], p3[1])
+            self.ctx.line_to(p3[0], p3[1] - circle_width)
+            self.ctx.arc(p3[0], p3[1], circle_width, math.pi/2*3, 0)
+            
         elif x == 4: # Top Left
-            self.ctx.arc(p3[0], p3[1] - circle_width, circle_width, math.pi/2*3, math.pi/2)
+            self.ctx.move_to(p1[0], p1[1])
+            self.ctx.line_to(p1[0], p1[1] + circle_width)
+            self.ctx.move_to(p1[0], p1[1])
+            self.ctx.line_to(p1[0] + circle_width, p1[1])
+            self.ctx.arc(p1[0], p1[1], circle_width, 0, math.pi/2)
+        
+        self.s_or_f()
+
     
     # Only changes colour sometimes to get funkier art
     def colour_switch(self):
         if randint(0, 1):
             self.ctx.set_source_rgba(random(), random(), random(), random())  # Solid color
+        
+    def s_or_f(self):
+        if randint(0, 1):
+            self.ctx.fill()
+        else:
+            self.ctx.stroke()
 
 if __name__ == '__main__': # Testing
-    Picture(10, 10)
+    Picture(6, 10)
